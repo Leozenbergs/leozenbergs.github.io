faf4a68e7a9df358be67120529546afd05c5b685
Leonardo Rodrigues <leoabreu14@gmail.com>
2019-05-13 22:37:44 -0300

    implementanto smooth links

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,14 +1,48 @@
 // AOS Library load
 AOS.init();
-var scroll = document.getElementById('scrollDown');
-scroll.addEventListener('click', function(){
-  // console.log('teste');
-  window.scrollTo({
-    top: 650,
-    behavior: 'smooth'
-  });
+let mainbutton = document.getElementById('scrollDown');
+let about = document.getElementById('link_about');
+let portifolio = document.getElementById('link_portifolio');
+let contact = document.getElementById('link_contact');
+
+
+mainbutton.addEventListener('click', function(){
+
+      var a = document.getElementById('about_me');
+      var medidaArrA = a.offsetTop;
+      console.log(medidaArrA);
+      scrollToElem(medidaArrA);
+});
+about.addEventListener('click', function(){
+
+      var b = document.getElementById('about_me');
+      var medidaArrB = b.offsetTop;
+      console.log(medidaArrB);
+      scrollToElem(medidaArrB);
+});
+portifolio.addEventListener('click', function(){
+
+      var c = document.getElementById('my_portifolio');
+      var medidaArrC = c.offsetTop;
+      console.log(medidaArrC);
+      scrollToElem(medidaArrC);
+});
+contact.addEventListener('click', function(){
+
+      var d = document.getElementById('contact_me');
+      var medidaArrD = d.offsetTop;
+      console.log(medidaArrD);      
+      scrollToElem(medidaArrD);
 });
 
+
+
+function scrollToElem(y){
+  window.scrollTo({
+    top: y,
+    behavior: 'smooth'
+  });
+}
 
 // Menu animation
 function headerAnimation(x) {
