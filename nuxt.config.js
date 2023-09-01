027187bd7c11e78d9e7519700daf1b8d9b0cfd13
Leonardo Rodrigues <contato@leonardorodrigues.me>
2023-09-01 19:28:29 -0300

    config

--- nuxt.config.js
+++ nuxt.config.js
@@ -3,7 +3,7 @@ import colors from 'vuetify/es5/util/colors'
 export default {
   // Disable server-side rendering: https://go.nuxtjs.dev/ssr-mode
   ssr: false,
-  mode: 'static',
+  target: 'static',
   router: {
     base: '/'
   },
