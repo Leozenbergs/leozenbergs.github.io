8c27283cc8fa5ced0a050b7d31a25cdbd75d6756
Leonardo Rodrigues <contato@leonardorodrigues.me>
2023-09-22 01:23:41 -0300

    feat: add animations

--- nuxt.config.js
+++ nuxt.config.js
@@ -4,6 +4,10 @@ export default {
   // Disable server-side rendering: https://go.nuxtjs.dev/ssr-mode
   ssr: false,
 
+  server: {
+    port: 3030 // default: 3000
+  },
+
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
     titleTemplate: '%s - Leonardo Rodrigues',
--- pages/index.vue
+++ pages/index.vue
@@ -1,15 +1,21 @@
 <template lang="md">
-  <v-container>
-    <div class="text-h3 text-center my-4">About me</div>
-    <v-skeleton-loader v-show="!$accessor.about" type="article, article" />
-    <div v-html='$md.render($accessor.about)'></div>
-  </v-container>
+  <kinesis-container>
+    <kinesis-element :strength="5" type="depth">
+      <v-container>
+        <div class="text-h3 text-center my-4">About me</div>
+        <v-skeleton-loader v-show="!$accessor.about" type="article, article" />
+        <div v-html='$md.render($accessor.about)'></div>
+      </v-container>
+    </kinesis-element>
+  </kinesis-container>
 </template>
 
 <script>
 import global from '~/mixins/globalMixin.vue'
+import { KinesisContainer, KinesisElement } from 'vue-kinesis'
 
 export default {
+  components: { KinesisContainer, KinesisElement },
   mixins: [global],
   mounted() {
     this.getMD()
